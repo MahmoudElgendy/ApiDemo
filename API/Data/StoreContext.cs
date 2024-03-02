0beec7a2e0018d307857d112d46afcc60a0393b7
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-01 23:10:27 -0600

    Create StoreContext and add Products Dbset to it

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
